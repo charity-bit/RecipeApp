45c5bf9140a2cf38f2929205b0818568793633af
user.name <charitynyanchera@gmail.com>
2021-08-14 23:53:29 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const searchForm = document.querySelector('form');
-
 const searchResultDiv = document.querySelector('.search-result');
 const container = document.querySelector('.container');
 let searchQuery = '';
-
+const APP_ID = "enter your own app id here";
+const APP_key = "enter your own app key Here";
 
 searchForm.addEventListener('submit',(e)=>{
 
@@ -18,5 +18,28 @@ async function fetchAPI(){
     const data = await response.json();
     generateHTML(data.hits)
     console.log(data);
+  }
+
+
+
+
+  function generateHTML(results){
+    container.classList.remove('initial');
+    let generatedHTML= '';
+    results.map(result => {
+      generatedHTML += `
+        <div class="item">
+          <img src="${result.recipe.image}" alt="img">
+          <div class="flex-container">
+            <h1 class="title">${result.recipe.label}</h1>
+            <a class="view-btn" target="_blank" href="${result.recipe.url}">View Recipe</a>
+          </div>
+          <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
+          <p class="item-data">Diet label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : 'No Data Found'}</p>
+          <p class="item-data">Health labels: ${result.recipe.healthLabels}</p>
+        </div>
+      `
+    })
+    searchResultDiv.innerHTML = generatedHTML;
   }
   
